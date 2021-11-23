474eea52227957b4df7de7ef8be0438d40f4c49b
Marcin Zyla <marcin.zyla13@gmail.com>
2021-11-23 14:25:13 +0100

    Add Healer class

---
+++ Assets/Source/Actors/Characters/Healer.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Source.Core;
+using UnityEngine;
+using DungeonCrawl.Core;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+namespace DungeonCrawl.Actors.Characters
+{
+    public class Healer : Character
+    {
+        private float DelayCounter { get; set; }
+
+        public Healer()
+        {
+        }
+        public override bool OnCollision(Actor anotherActor)
+        {
+            if (anotherActor is Player)
+            {
+                Player player = (Player)anotherActor;
+                player.CurrentHealth = player.MaxHealth;
+                UserInterface.Singleton.ShowFightScreen(player, this);
+            }
+            return false;
+        }
+        protected override void OnDeath() { }
+
+
+
+        public override int DefaultSpriteId { get; set; } = 78;
+        public override string DefaultName => "Healer";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
